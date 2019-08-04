09e7aa7b7a296d5ca72a3eb3dc817b3e209f45bc
KK66 <kilian.kellermann@yahoo.com>
2019-08-04 14:19:59 +0200

    Init ColorSwitch-Gameapp

---
+++ ColorSwitch/ColorSwitch/ViewController/GameViewController.swift
@@ -0,0 +1,34 @@
+//
+//  GameViewController.swift
+//  ColorSwitch
+//
+//  Created by Kilian Kellermann on 30.07.19.
+//  Copyright © 2019 Kilian Kellermann. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            let scene = MenuScene(size: view.bounds.size)
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+                
+            // Present the scene
+            view.presentScene(scene)
+            
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+}
